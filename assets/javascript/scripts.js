34717a2ad132fa9273da56c31e06152775fc2347
smellyalater <acopleman@gmail.com>
2016-01-14 00:51:51 -0500

    new homepage using bourbon

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
-  console.log('page ready and js working');
+  
+
+//BEGIN responsive mobile menu - this collapses the top nav into a mobile ready menu
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+//END responsive mobile menu - this collapses the top nav into a mobile ready menu
+
+//BEGIN homepage content js
+
+$('.accordion-tabs').each(function() {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+  $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+
+//END homepage content js
+
+
+
+
+
 });
